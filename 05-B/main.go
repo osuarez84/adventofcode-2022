4df33c0d7490cdda50ee29b8d89d6d219e52b18f
osuarez <omsulo@gmail.com>
2022-12-07 11:04:50 +0100

    add code for 5B

---
+++ 05-B/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	lls "github.com/emirpasic/gods/stacks/linkedliststack"
+)
+
+
+const (
+	stack1 = "SPWNJZ"
+	stack2 = "TSG"
+	stack3 = "HLRQV"
+	stack4 = "DTSV"
+	stack5 = "JMBDTZQ"
+	stack6 = "LZCDJTWM"
+	stack7 = "JTGWMPL"
+	stack8 = "HQFBTMGN"
+	stack9 = "WQBPCGDR"
+)
+
+
+func createTheStacks() []*lls.Stack {
+	initStacks := []string{
+		"ZJNWPS",
+		"GST",
+		"VQRLH",
+		"VSTD",
+		"QZTDBMJ",
+		"MWTJDCZL",
+		"LPMWGTJ",
+		"NGMTBFQH",
+		"RDGCPBQW",
+	}
+
+	var tmpListOfStacks []*lls.Stack
+	var tmpStack *lls.Stack
+	tmpListOfStacks = append(tmpListOfStacks, lls.New()) // first null stack
+	for _, el := range initStacks {
+		tmpStack = lls.New()
+		for _, char := range el {
+			tmpStack.Push(string(char))
+		}
+		tmpListOfStacks = append(tmpListOfStacks, tmpStack)
+	}
+
+	return tmpListOfStacks
+}
+
+func getInputLines(name string) (*bufio.Scanner, error) {
+	readFile, err := os.Open("input.txt")
+	if err != nil {
+		return nil, err
+	}
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	return fileScanner, nil
+}
+
+func getMovValues(text string) (int, int, int, error) {
+	re := regexp.MustCompile("move ([0-9]+) from ([0-9]+) to ([0-9]+)")
+	parts := re.FindStringSubmatch(text)
+
+	numberCrates, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return 0, 0, 0, err
+	}
+	fromStack, err := strconv.Atoi(parts[2])
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+	toStack, err := strconv.Atoi(parts[3])
+	if err != nil {
+		return 0, 0, 0, err
+	}
+
+
+	return numberCrates, fromStack, toStack , nil
+}
+
+
+func executeMovements(numCrates, from, to int, lStacks []*lls.Stack) {
+	temporalStack := lls.New()
+
+	// Move from init to temporal stack
+	for i := 0; i < numCrates; i++ {
+		tmp, isAnything := lStacks[from].Pop()
+		if isAnything {
+			temporalStack.Push(tmp)
+			fmt.Printf("Moving %s from %d to temporal stack\n", tmp, from)
+		} else {
+			fmt.Println("Stack empty, nothing to move.")
+		}
+	}
+	fmt.Println("Temporal stack: ", temporalStack)
+	fmt.Println("Temporal stack size: ", temporalStack.Size())
+
+	// Move from temporal to the final stack
+	temporalStackSize := temporalStack.Size()
+	for i := 0; i < temporalStackSize; i++ {
+		tmp, _ := temporalStack.Pop()
+		lStacks[to].Push(tmp)
+		fmt.Printf("Moving %s from temporal stack to %d\n", tmp, to)
+	}
+
+}
+
+func main() {
+	listOfStacks := createTheStacks()
+	fmt.Println(listOfStacks)
+
+	lines, err := getInputLines("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	cont := 10
+	for lines.Scan() {
+		if cont > 0 {
+			cont-- // skip the first 10 lines
+		} else {
+			numberCrates, fromStack, toStack, err := getMovValues(lines.Text())
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(numberCrates, fromStack, toStack)
+			executeMovements(numberCrates, fromStack, toStack, listOfStacks)
+		}
+
+	}
+	fmt.Println("The final stacks are: ", listOfStacks)
+
+	// Get the top of every Stack
+	var topStackCrates []string
+	for _, stack := range listOfStacks {
+		tmp, isAnything := stack.Peek()
+		if isAnything {
+			topStackCrates = append(topStackCrates, tmp.(string))
+		}
+	}
+	fmt.Println("The top list of crates: ", strings.Join(topStackCrates, ""))
+}
